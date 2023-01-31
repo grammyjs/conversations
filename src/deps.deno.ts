80c5bb48a39f000514a625dbe23e4db9fc2bbd63
evermake <vladeryabkin@gmail.com>
2023-01-31 11:06:00 +0300

    fix: update oson dependency to fix constructor.name error

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -2,6 +2,6 @@ export {
     delistify,
     GLOBAL_CONSTRUCTOR_MAP,
     listify,
-} from "https://deno.land/x/oson@1.0.0/mod.ts";
+} from "https://deno.land/x/oson@1.0.1/mod.ts";
 export * from "https://lib.deno.dev/x/grammy@v1/mod.ts";
 export * from "https://lib.deno.dev/x/grammy@v1/types.ts";
