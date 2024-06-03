f65b4d2b5500c1a2a897e88218710b445d410df3
KnorpelSenf <shtrog@gmail.com>
2024-06-03 11:43:25 +0200

    fix: list deps explicitly

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,2 +1,13 @@
-export * from "https://lib.deno.dev/x/grammy@v1/mod.ts";
-export * from "https://lib.deno.dev/x/grammy@v1/types.ts";
+export {
+    Api,
+    Composer,
+    Context,
+    GrammyError,
+    HttpError,
+    type Middleware,
+} from "https://lib.deno.dev/x/grammy@v1/mod.ts";
+export type {
+    ApiError,
+    Update,
+    UserFromGetMe,
+} from "https://lib.deno.dev/x/grammy@v1/types.ts";
