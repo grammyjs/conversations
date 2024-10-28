afc9cb70a5c2c78af7ed2a5d6324780447ae801d
KnorpelSenf <shtrog@gmail.com>
2024-10-28 21:43:34 +0100

    feat: export all menu symbols

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,7 @@
 export * from "./conversation.ts";
 export * from "./engine.ts";
 export * from "./form.ts";
+export * from "./menu.ts";
 export * from "./plugin.ts";
 export * from "./resolve.ts";
 export * from "./state.ts";
