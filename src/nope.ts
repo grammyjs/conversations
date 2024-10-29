78b3ce9923b8e81d08430ed1e38cbc456422bddf
KnorpelSenf <shtrog@gmail.com>
2024-10-29 20:35:00 +0100

    docs: document nope.ts

--- src/nope.ts
+++ src/nope.ts
@@ -1,3 +1,18 @@
+/**
+ * Creates an object that throws an error when touched in any way. This includes
+ *
+ * - getting a property
+ * - setting a property
+ * - calling the object
+ * - constructing the object
+ *
+ * and any of the other [object internal
+ * methods](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy#object_internal_methods)
+ * in JavaScript.
+ *
+ * @param msg An error message to use
+ * @typeParam The type of object to create
+ */
 export function youTouchYouDie<T extends object>(msg: string) {
     function nope(): never {
         throw new Error(msg);
